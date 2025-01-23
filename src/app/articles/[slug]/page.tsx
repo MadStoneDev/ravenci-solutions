96d70e39c7e7a1a5941125119ba7f052721c632d
Richard H. <richard@haddads.net.au>
2025-01-24 00:16:03 +1000

    added link back to articles in single article page

--- src/app/articles/[slug]/page.tsx
+++ src/app/articles/[slug]/page.tsx
@@ -1,4 +1,6 @@
 ﻿import { getArticleBySlug } from "@/lib/article";
+import Link from "next/link";
+import { IconArrowLeft } from "@tabler/icons-react";
 
 function formatContent(content: any[]) {
   return content
@@ -40,6 +42,17 @@ export default async function ArticlePage({
         className={`content-section py-32 px-5 sm:px-20 xl:px-36 grid justify-center min-h-[250px] bg-white`}
       >
         <article className={`col-span-12 flex flex-col max-w-3xl`}>
+          <Link
+            href={`/articles`}
+            className={`group relative mb-4 pl-1 pr-2 py-0.5 inline-flex items-center gap-2 w-fit hover:scale-110 font-bold text-sm text-ravenci-primary hover:text-white transition-all duration-300 ease-in-out`}
+          >
+            <IconArrowLeft size={18} className={`z-10`} />
+            <span className={`z-10`}>Back to Articles</span>
+
+            <div
+              className={`absolute top-0 left-full group-hover:left-0 right-0 bottom-0 bg-ravenci-primary transition-all duration-300 ease-in-out`}
+            ></div>
+          </Link>
           <h1 className="text-4xl font-bold mb-6">{article.Title}</h1>
           <div className="prose lg:prose-xl">
             {formatContent(article.Content)}
