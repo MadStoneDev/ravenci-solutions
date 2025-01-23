8817f92b1017b6e58fe312c3e4b5fa4a6564e1e8
Richard H. <richard@haddads.net.au>
2025-01-23 12:32:03 +1000

    add microsoft clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 
 import { Lexend } from "next/font/google";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import Script from "next/script";
 
 import "./globals.css";
 import MainNavigation from "@/components/main-navigation";
@@ -34,6 +35,14 @@ export default function RootLayout({
         <MainFooter />
 
         <SpinningCircleText />
+
+        <Script id={`clarity-script`} strategy={`afterInteractive`}>
+          {`(function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "py6lstoq0x");`}
+        </Script>
       </body>
     </html>
   );
