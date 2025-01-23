f545907562eca8d26fd1d775e6ab9e784712d81f
Richard H. <richard@haddads.net.au>
2025-01-23 17:15:18 +1000

    installed axios

---
+++ src/app/articles/[slug]/page.tsx
@@ -0,0 +1,3 @@
+﻿export default function ArticlePage({ params }: { params: { slug: string } }) {
+  return <div>Article Page: {params.slug}</div>;
+}
---
+++ src/app/articles/page.tsx
@@ -0,0 +1,11 @@
+﻿export default function ArticlesPage() {
+  return (
+    <main className={`flex flex-col`}>
+      <section
+        className={`content-section py-32 px-5 sm:px-20 xl:px-36 grid grid-cols-12 gap-6 justify-center min-h-[750px] bg-white`}
+      >
+        <div>Articles</div>
+      </section>
+    </main>
+  );
+}
