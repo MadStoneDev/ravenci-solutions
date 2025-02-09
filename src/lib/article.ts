f29c52f41ee6e20d11caa5d12377feab2c9d839d
Richard H. <richard@haddads.net.au>
2025-02-09 23:12:33 +1000

    fix getArticleBySlug for firefox

--- src/lib/article.ts
+++ src/lib/article.ts
@@ -5,20 +5,29 @@ const CACHE_TIME = 7 * 24 * 60 * 60; // 1 week
 
 export async function getArticleBySlug(slug: string) {
   const cacheKey = `article:${slug}`;
-  const cached = await redis.get<string>(cacheKey);
 
-  if (cached) return cached;
+  try {
+    const cached = await redis.get<string>(cacheKey);
+    if (cached) return JSON.parse(cached);
 
-  const response = await axios.get(
-    `https://strapi.ravenci.solutions/api/articles?filters[Slug][$eq]=${slug}&populate=*`,
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.STRAPI_API_KEY}`,
+    const response = await axios.get(
+      `https://strapi.ravenci.solutions/api/articles?filters[Slug][$eq]=${slug}&populate=*`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.STRAPI_API_KEY}`,
+        },
       },
-    },
-  );
+    );
 
-  const article = response.data.data ? response.data.data[0] : false;
-  await redis.set(cacheKey, article, { ex: CACHE_TIME });
-  return article;
+    const article = response.data.data ? response.data.data[0] : false;
+
+    if (article) {
+      await redis.set(cacheKey, JSON.stringify(article), { ex: CACHE_TIME });
+    }
+
+    return article;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
 }
